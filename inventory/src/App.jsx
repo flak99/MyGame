59575f6d1658e786fbabea9079b3e70b0adbf115
inż. Krystian Flak <flak99@o2.pl>
2025-09-27 20:51:01 +0200

    Pierwsze proby tworzenia systemy ekwipunku

---
+++ inventory/src/App.jsx
@@ -0,0 +1,61 @@
+import "./App.css";
+
+export function App() {
+  return (
+    <>
+      <div className="container">
+        <div className="main-area">
+          <table className="mainEQ">
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+          </table>
+          <table className="inventory">
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+          </table>
+        </div>
+      </div>
+    </>
+  );
+}
