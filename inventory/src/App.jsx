761b175486f4ddfecdaa4c692e377e024da1283b
inż. Krystian Flak <flak99@o2.pl>
2025-09-28 21:47:04 +0200

    Rozpoczecie pracy z react Dnd

--- inventory/src/App.jsx
+++ inventory/src/App.jsx
@@ -1,21 +1,37 @@
 import "./App.css";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 
 export function App() {
+  const items = [
+    { itemName: "Broń", liczba: 15 },
+    { itemName: "Mikstura", liczba: 20 },
+    { itemName: "Tarcza", liczba: 10 },
+  ];
+
+  function ShowItems(array) {
+    return array.map((e, index) => (
+      <div className="element" key={index}>
+        {" "}
+        {e.itemName}{" "}
+      </div>
+    ));
+  }
+
   return (
     <>
-      <div className="container">
-        <div className="main-area">
-          <table className="mainEQ">
-            <tbody>
-              <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-              </tr>
-            </tbody>
-          </table>
+      <DndProvider backend={HTML5Backend}>
+        <div className="base">
+          <div className="contianer">
+            <p>Inventory</p>
+            <div className="ItemsArray">{ShowItems(items)}</div>
+          </div>
+          <div className="contianer">
+            <p>Active Items</p>
+            <div className="ItemsArray "></div>
+          </div>
         </div>
-      </div>
+      </DndProvider>
     </>
   );
 }
